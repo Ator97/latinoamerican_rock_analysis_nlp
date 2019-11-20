f6c3f3b8f32c1ddd8884832453637692119202d1
Ator97 <oscargutcas9705@gmail.com>
2019-11-20 12:53:02 -0600

    Obtencion de reduccion de letras de canciones

--- scripts/p2_load_clean_songs.py
+++ scripts/p2_load_clean_songs.py
@@ -1,7 +1,9 @@
 import os, json
 import pandas as pd
 import re
-path_to_json = '/home/ator97/Documents/git/npl_music/artist'
+import subprocess
+master_path='/home/ator97/Documents/git/npl_music/'
+path_to_json = master_path+'artist'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 jsons_data = pd.DataFrame(columns=['title', 'album', 'year','lyrics','artist'])
 for index, js in enumerate(json_files):
@@ -19,15 +21,30 @@ for i in range(0,7954):
     p0 = re.sub( '\n'                                                                               , ' ', jsons_data.at[i,'lyrics'].lower()) 
     p1 = re.sub( '\[[\*¿?!¡\[\]\+\-\"\'\(\)\/\+¿?!¡\.\"\:,;\/\\\'\&$“”’öüñèáéíóúa-z0-9 ]+\]'        , ' ',p0   )
     p2 = re.sub( '\([°\*—\[\]\+\-\"\'\(\)\/\+¿?!¡\.\"\:,;\/\\\'\&$“”’öüñèáéíóú…´a-z0-9 ]+\)'       , ' ',p1   )
-    p3 = re.sub( '[\(\)\[\]\+¿?!¡\.\":,;\/\\\'-=»«]+'                                                 , ' ',p2   )
+    p3 = re.sub( '[\(\)\[\]\+¿?!¡\.\":,;\/\\\'-=»«]+'                                                , ' ',p2   )
     p4 = re.sub( ','                                                                                , ' ',p3   )
     clean_song.append(re.sub('[ ]+', ' ', p4.strip()))
     
 #Agregamos cancion limpia
 jsons_data["cleanSongs"]=clean_song
 #Eliminamos todas las canciones con letras muy pequeñas o inexistentes
-jsons_data = jsons_data.loc[ jsons_data['cleanSongs'].str.split().str.len() > 20] 
+jsons_data = jsons_data.loc[ jsons_data['cleanSongs'].str.split().str.len() > 30] 
 #Dejamos solo el año de release
 jsons_data['year'] = jsons_data['year'].str.slice(0,4)
 jsons_data = jsons_data[['artist','album','year','title','cleanSongs']]
-export_csv = jsons_data.to_csv('/home/ator97/Documents/git/npl_music/clean_songs.cvs')
+#Reinicio de indice
+jsons_data = jsons_data.reset_index()
+del jsons_data['index']
+#Getting compression level
+songs = jsons_data[['cleanSongs']]
+compression_rate=[]
+#
+for i in range (0,len(songs)):
+    f=open(master_path+'compress/'+str(i)+'.txt',"w+")
+    f.write(songs.at[i,'cleanSongs'])
+    f.close()
+    os.system("gzip "+master_path+'compress/'+str(i)+'.txt')
+    compression_rate.append( subprocess.check_output("gzip -l "  +master_path+"compress/"+str(i)+".txt.gz"+  " | tail -1 | awk {'print $3'}", shell=True)[:-2].decode("utf-8") )
+    os.system("rm "+ master_path+"compress/"+str(i)+".txt.gz")
+jsons_data["compression_rate"]=compression_rate
+export_csv = songs.to_csv(master_path+'clean_songs.cvs')
